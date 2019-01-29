e597ee1b4a7a612b4299c0f6f91bd4aa6fef7e4c
tanner gaucher <tannermichaelgaucher@gmail.com>
2019-01-29 15:56:05 +0800

    remove grommet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { createGlobalStyle } from 'styled-components'
-import { Grommet } from 'grommet'
 
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
